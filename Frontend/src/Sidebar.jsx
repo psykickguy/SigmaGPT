ee4081726a028b27d1266ed6f250ba31afb41089
PsyKickGuy <shubhdas2005@gmail.com>
2025-08-10 00:12:56 +0530

    fixed image issue

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -85,11 +85,7 @@ function Sidebar() {
   return (
     <section className="sidebar">
       <button>
-        <img
-          src="src/assets/blacklogo.png"
-          alt="gpt logo"
-          className="logo"
-        ></img>
+        <img src="./blacklogo.png" alt="gpt logo" className="logo"></img>
         <span>
           <i className="fa-solid fa-pen-to-square"></i>
         </span>
